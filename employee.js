46675051391696243db4ae0909417c71b9c89981
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-09-07 18:19:49 -0400

    working on the questions as well as the backend to help set up the charts

--- employee.js
+++ employee.js
@@ -33,7 +33,6 @@ function mainMenu() {
                 "View all departments",
                 "View all roles",
                 "View all employees",
-                "View all roles",
                 "Add a department",
                 "Add a role",
                 "Add an employee",
@@ -43,44 +42,73 @@ function mainMenu() {
                 connectionLevels.query("SELECT * FROM department", (err, results) => {
                     if (err) {
                         return console.log(err.message);
-                    }console.log(results)
+                    } console.log(results)
                     return results;
                 })
-            }else if (answers.action === "Add a department") {
-                connectionLevels.query("INSERT INTO department SET ? ", {name: "Accounting"}, (err, results) => {
-                    if(err) {
+            } else if (answers.action === "Add a department") {
+                connectionLevels.query("INSERT INTO department SET ? ", { name: "Accounting" }, (err, results) => {
+                    if (err) {
                         return console.log(err.message);
-                    }console.log(results);
+                    } console.log(results);
                     return results;
                 })
-            }if (answers.action === "View all roles") {
+            } if (answers.action === "View all roles") {
                 connectionLevels.query("SELECT * FROM role", (err, results) => {
                     if (err) {
                         return console.log(err.message);
-                    }console.log(results)
+                    } console.log(results)
                     return results
                 })
-            }else if (answers.action === "Add a role") {
-                connectionLevels.query("SELECT FROM * department ", (err, results) => {
+            } else if (answers.action === "Add a role") {
+                connectionLevels.query("SELECT * FROM department", (err, results) => {
                     if (err) throw err;
 
                     inquirer
-                    .prompt([
-                        {
-                            name: "new_role",
-                            type: "input",
-                            message: "Please add the new role!",
-                        },
-                        {
-                            name: "salary",
-                            type: "input",
-                            message: "What is the salary of the new ${new_role} ?"
-                            
-                        }
-                    ])
+                        .prompt([
+                            {
+                                name: "new_role",
+                                type: "input",
+                                message: "Please add the new role!",
+                            },
+                            {
+                                name: "salary",
+                                type: "input",
+                                message: `What is the salary of the new ${new_role}?`
+
+                            },
+                            {
+                                name: "department_id",
+                                type: "input",
+                                message: "What is the department of this new role?",
+                            }
+                        ]).then(answers => {
+                            const role = answers.new_role
+                            const salary = answers.salary
+                            const department_id = answers.department_id
+                            connectionLevels.query(`INSERT INTO role SET (new_role, salary, department_id) VALUES "${role}", "${salary}", "${department_id}"`);
+                            if (err) {
+                                throw err
+                            } console.table(answers)
+                        })
 
                 })
-            
+
+            }if (answers.action === "View All Employees") {
+                connectionLevels.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name")
+                if (err) {
+                    return console.log(err.message)
+                }console.log(results)
+                return results
+            }else if (answers.action === "Add an employee") {
+                connectionLevels.query("INSERT INTO employee", (err, results) => {
+                    if (err) throw err
+
+                    inquirer
+                    .prompt([
+                                
+                    ])
+                })
+
             }
 
         })
