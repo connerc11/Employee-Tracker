3bd135dedb3e34628f77a7d010e2c8b90ec9e2c6
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-09-11 14:06:59 -0400

    worked on moving functions out of the original if statements to help get main menu

--- employee.js
+++ employee.js
@@ -1,6 +1,7 @@
 const { EEXIST } = require("constants");
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+const { allowedNodeEnvironmentFlags } = require("process");
 const util = require("util");
 
 const connectionLevels = mysql.createConnection({
@@ -22,7 +23,14 @@ connectionLevels.query("SELECT * FROM department", (err, results) => {
   return results;
 });
 
-function mainMenu() {
+async function mainMenu() {
+    // const departments = await viewDepartments()
+    // const departmentArr = departments.map(({id, name}) => ({
+    //     name: name, 
+    //     value: id
+    // }) )
+    
+    
   inquirer
     .prompt({
       name: "action",
@@ -40,127 +48,22 @@ function mainMenu() {
     })
     .then((answers) => {
       if (answers.action === "View all departments") {
-        connectionLevels.query("SELECT * FROM department", (err, results) => {
-          if (err) {
-            return console.log(err.message);
-          }
-          console.log(results);
-          return results;
-        });
+        viewDepartments();
       } if (answers.action === "Add a department") {
-        connectionLevels.query(
-          "INSERT INTO department SET ? ",
-          { name: "Accounting" },
-          (err, results) => {
-            if (err) {
-              return console.log(err.message);
-            }
-            console.log(results);
-            return results;
-          }
-        );
+        addDepartment();
       }
       if (answers.action === "View all roles") {
-        connectionLevels.query("SELECT * FROM role", (err, results) => {
-          if (err) {
-            return console.log(err.message);
-          }
-          console.log(results);
-          return results;
-        });
+       viewRole();
       } if (answers.action === "Add a role") {
-        connectionLevels.query("SELECT * FROM department", (err, results) => {
-          if (err) throw err;
-
-          inquirer
-            .prompt([
-              {
-                name: "new_role",
-                type: "input",
-                message: "Please add the new role!",
-              },
-              {
-                name: "salary",
-                type: "input",
-                message: `What is the salary of the new salary?`,
-              },
-              {
-                name: "department_id",
-                type: "input",
-                message: "What is the department of this new role?",
-              },
-            ])
-            .then((answers) => {
-              const role = answers.new_role;
-              const salary = answers.salary;
-              const department_id = answers.department_id;
-              connectionLevels.query(
-                `INSERT INTO role SET (new_role, salary, department_id) VALUES "${role}", "${salary}", "${department_id}"`,
-                (err,
-                (results) => {
-                  if (err) {
-                    throw err;
-                  }
-                })
-              );
-              console.table(results);
-            });
-        });
+        addRole();
       }
       if (answers.action === "View all employees") {
-        connectionLevels.query(
-          "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id FROM employee",
-          (err, results) => {
-            if (err) {
-            return console.log(err.message);
-          }
-          console.log(results);
-          return results;
-        }
-        );
-      }if (answers.action === "Add an employee") {
-        connectionLevels.query("INSERT INTO employee", (err, results) => {
-          if (err) throw err;
 
-          inquirer
-            .prompt([
-              {
-                type: "input",
-                message: "Please enter the employee's first name",
-                name: "first_name",
-              },
-              {
-                type: "input",
-                message: "Please enter the employee's last name",
-                name: "last_name",
-              },
-              {
-                type: "input",
-                message: "Please enter the employee's role",
-                name: "role_id",
-              },
-              {
-                type: "input",
-                message: "Please enter the employee's manager",
-                name: "manager_id",
-              },
-            ])
-            .then((answers) => {
-              const first_name = answers.first_name;
-              const last_name = answers.last_name;
-              const role_id = answers.role_id;
-              const manager_id = answers.manager_id;
-              connectionLevels.query(
-                `INSERT INTO employee.first_name, employee.last_name, employee.role_id, employee.manager_id VALUES ${first_name} ${last_name} ${role_id} ${manager_id}`,
-                (err, results) => {
-                  if (err) throw err;
-                  return results;
-                }
-              );
-            });
-        });
+        viewEmployees();
+      }if (answers.action === "Add an employee") {
+        addEmployee();
       }
-      if (answers.action === "Update an employee role")
+      if (answers.action === "Update an employee role") 
         inquirer
           .prompt([
             {
@@ -173,6 +76,7 @@ function mainMenu() {
               message: "Enter the new role ID for the employee!",
               name: "UpdateRole",
             },
+        
           ])
           .then((answers) => {
             const updateE = answers.updateE;
@@ -187,7 +91,152 @@ function mainMenu() {
             );
           });
     });
-}
+    }
+
 //
+
+
+const viewRole = () => {
+    connectionLevels.query("SELECT * FROM role", (err, results) => {
+        if (err) {
+          return console.log(err.message);
+        }
+        console.log(results);
+        return results;
+      });
+}
+
+const addRole = () => {
+    connectionLevels.query("SELECT * FROM department", (err, results) => {
+        if (err) throw err;
+
+        inquirer
+          .prompt([
+            {
+              name: "new_role",
+              type: "input",
+              message: "Please add the new role!",
+            },
+            {
+              name: "salary",
+              type: "input",
+              message: `What is the salary of the new salary?`,
+            },
+            {
+              name: "department_id",
+              type: "list",
+              message: "What is the department of this new role?",
+              // choices: departmentArr,
+            },
+          ])
+          .then((answers) => {
+            const role = answers.new_role;
+            const salary = answers.salary;
+            const department_id = answers.department_id;
+            connectionLevels.query(
+              `INSERT INTO role SET (new_role, salary, department_id) VALUES "${role}", "${salary}", "${department_id}"`,
+              (err,
+              (results) => {
+                if (err) {
+                  throw err;
+                }
+              })
+            );
+            mainMenu();
+            console.table(results);
+          });
+      });
+}
+
+const addDepartment = () => {
+    connectionLevels.query(
+        "INSERT INTO department SET ? ",
+        { name: "Accounting" },
+        (err, results) => {
+          if (err) {
+            return console.log(err.message);
+          }
+          console.log(results);
+          mainMenu();
+          return results;
+        }
+      );
+    }
+
+
+
+const viewEmployees = () => {
+    return connectionLevels.query(
+        "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id FROM employee",
+        (err, results) => {
+          if (err) {
+          return console.log(err.message);
+        }
+        console.log(results);
+        mainMenu();
+        return results;
+      }
+      );
+}
+
+const addEmployee = () => {
+    connectionLevels.query("INSERT INTO employee", (err, results) => {
+        if (err) throw err;
+
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "Please enter the employee's first name",
+              name: "first_name",
+            },
+            {
+              type: "input",
+              message: "Please enter the employee's last name",
+              name: "last_name",
+            },
+            {
+              type: "input",
+              message: "Please enter the employee's role",
+              name: "role_id",
+            },
+            {
+              type: "input",
+              message: "Please enter the employee's manager",
+              name: "manager_id",
+            },
+          ])
+          .then((answers) => {
+            const first_name = answers.first_name;
+            const last_name = answers.last_name;
+            const role_id = answers.role_id;
+            const manager_id = answers.manager_id;
+            connectionLevels.query(
+              `INSERT INTO employee.first_name, employee.last_name, employee.role_id, employee.manager_id VALUES ${first_name} ${last_name} ${role_id} ${manager_id}`,
+              (err, results) => {
+                if (err) throw err;
+                mainMenu();
+                return results;
+              }
+            );
+          });
+      });
+}
+
+
+const viewDepartments = () => {
+    return connectionLevels.query("SELECT * FROM department", (err, results) => {
+        if (err) {
+          return console.log(err.message);
+        }
+        console.log(results);
+        mainMenu()
+        return results;
+        
+      });
+     
+}
+
+
 
 mainMenu();
--- seeds.sql
+++ seeds.sql
@@ -1,2 +1,4 @@
-USE TABLE departments
-
+USE employee_trackersDB
+INSERT INTO department(name) VALUES("Accounting"),("HR"),("Sales"),("Engineering");
+INSERT INTO role(title, salary, department_id) VALUES("Accountant", 50000, 1),("HR VP", 60000, 2),("SALESMAN/SALESWOMAN", 50000, 3),("Engineer", 70000, 4);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES("Ted", "Peters", 2, NULL), ("Valeria", "Sanchez", 1, 1), ("Micheal", "Harris", 4, 1), ("Sarah", "Richards", 3, 3);
